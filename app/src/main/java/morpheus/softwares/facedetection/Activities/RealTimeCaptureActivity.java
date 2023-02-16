aeadf24f76f0ca7f1e686358869679b70cb43a94
mopheshi <ndachimya@gmail.com>
2023-02-16 21:43:55 +0100

    ...in progress!

--- app/src/main/java/morpheus/softwares/facedetection/Activities/RealTimeCaptureActivity.java
+++ app/src/main/java/morpheus/softwares/facedetection/Activities/RealTimeCaptureActivity.java
@@ -1,16 +1,26 @@
 package morpheus.softwares.facedetection.Activities;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import morpheus.softwares.facedetection.R;
 
 public class RealTimeCaptureActivity extends AppCompatActivity {
+    private static final int VIDEO_CAPTURE = 500;
+    private VideoView videoView;
+    private Button capture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_real_time_capture);
+
+        videoView = findViewById(R.id.videoView);
+        capture = findViewById(R.id.capture);
+
+
     }
 }
